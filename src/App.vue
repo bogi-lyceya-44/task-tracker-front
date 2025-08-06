a3265ee36375640c508edfc0b155fc6b4eedc1b0
PetrAhtimirov <petr.ahtimirov@gmail.com>
2025-08-06 23:53:28 +0300

    app init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Test from './components/Test.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Test
+  }
+});
+</script>
+
+<template>
+  <Test/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Test"
+})
+</script>
+
+<template>
+  Task tracker app
+</template>
+
+<style scoped>
+</style>
